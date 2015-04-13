0d65d61c85eead9fa5141e097cc456fc685b65c2
0c0b <sge3un@gmail.com>
2015-04-13 20:37:42 +0900

    day 10 -오후시간3

--- js/day10.js
+++ js/day10.js
@@ -1,31 +1,31 @@
 jQuery(function($){
 	//****************************trigger 이벤트 예제****************************
-	$('li').on('click',function(event){
-		$(this).clone().insertAfter(this);
-	});
-	$('#cloneItem').on('click', function(event){
-		$('li:first').trigger('click');
-	});
-	$('ul').on('click', 'li', function(event){
-		$(this).clone().insertAfter(this);
-	});
+		/*	$('li').on('click',function(event){
+				$(this).clone().insertAfter(this);
+			});
+			$('#cloneItem').on('click', function(event){
+				$('li:first').trigger('click');
+			});
+			$('ul').on('click', 'li', function(event){
+				$(this).clone().insertAfter(this);
+			});*/
 
 
 	//****************************박스 숨기기 예제****************************
-	$('#hide').on('click', function(event){
-		$('#box').delay(1000).animate({'opacity':0,'width':0}, 300);
-		//$('#box').fadeOut([300]).slideDown([300]);//애니메이션을 여러개를 동시에 쓰면 쌓인다. 처음것을 실행한 다음 바로 다음거를 실행한다.
-		//$('#box').hide([300]);
-	});
-	$('#show').on('click', function(event){
-		$('#box').fadeIn([300]);
-		//$('#box').show([300]);
-	});
-	$('#toggle').on('click', function(event){
-		$('#box').stop().fadeToggle([300]);
-		//$('#box').toggle([300]);
-	});
-
+		/*	$('#hide').on('click', function(event){
+				$('#box').delay(1000).animate({'opacity':0,'width':0}, 300);
+				//$('#box').fadeOut([300]).slideDown([300]);//애니메이션을 여러개를 동시에 쓰면 쌓인다. 처음것을 실행한 다음 바로 다음거를 실행한다.
+				//$('#box').hide([300]);
+			});
+			$('#show').on('click', function(event){
+				$('#box').fadeIn([300]);
+				//$('#box').show([300]);
+			});
+			$('#toggle').on('click', function(event){
+				$('#box').stop().fadeToggle([300]);
+				//$('#box').toggle([300]);
+			});
+*/
 
 
 //****************************키보드를 누르면 박스가 움직인다. 애니메이션으로 ****************************
@@ -49,66 +49,66 @@ jQuery(function($){
 	// });	
 
 
-//****************************키보드를 누르면 박스가 움직인다. 애니메이션으로 ****************************
-	$(document).on('keydown', function(event){
-		var $box = $('#box');
-		var windowW = $(window).width();
-		//var h2H = h2Offset.height();
-		//내가 했던것 ...
-		// var startLeft = parseInt($box.css('left'));
-		// var startTop  = parseInt($box.css('top'));
-		//console.log(startTop, startLeft);
-		var offset = $box.offset(), left = offset.left, top = offset.top;
-		var h2offset = $('h2:first').offset(), h2top = h2offset.top;
-			switch (event.which){
-				case 37: //왼쪽
-					if (left - 20 < 0){
-						left = 0;
-					} else {
-						left = left - 20;
+	//****************************키보드를 누르면 박스가 움직인다. 애니메이션으로 ****************************
+		/*	$(document).on('keydown', function(event){
+				var $box = $('#box');
+				var windowW = $(window).width();
+				//var h2H = h2Offset.height();
+				//내가 했던것 ...
+				// var startLeft = parseInt($box.css('left'));
+				// var startTop  = parseInt($box.css('top'));
+				//console.log(startTop, startLeft);
+				var offset = $box.offset(), left = offset.left, top = offset.top;
+				var h2offset = $('h2:first').offset(), h2top = h2offset.top;
+					switch (event.which){
+						case 37: //왼쪽
+							if (left - 20 < 0){
+								left = 0;
+							} else {
+								left = left - 20;
+							};
+							$box.stop().animate({'left': left}, 50);
+							break;
+						case 38: //위쪽
+							if (top - 20 < 0){
+								top = 0;
+							} else {
+								top = top - 20;
+							};
+							$box.stop().animate({'top': top}, 50);
+							break;
+						case 39: //오른쪽
+						//박스 오른쪽 모서리의 위치 (left+50)
+						//박스 아래 모서리 위치 (top-50)
+							if (left+50 > windowW){
+								left = windowW;
+							} else {
+								left = left +20;
+							};
+								//event.preventDefault();
+							$box.stop().animate({'left': left}, 50);
+							break;
+							case 40: //아래쪽
+							if (top+50 > h2top){
+								top = h2top;
+							} else {
+								top = top + 50;
+								//event.preventDefault();
+							};
+							$box.stop().animate({'top': top}, 50);
+							break;
 					};
-					$box.stop().animate({'left': left}, 50);
-					break;
-				case 38: //위쪽
-					if (top - 20 < 0){
-						top = 0;
-					} else {
-						top = top - 20;
-					};
-					$box.stop().animate({'top': top}, 50);
-					break;
-				case 39: //오른쪽
-				//박스 오른쪽 모서리의 위치 (left+50)
-				//박스 아래 모서리 위치 (top-50)
-					if (left+50 > windowW){
-						left = windowW;
-					} else {
-						left = left +20;
-					};
-						//event.preventDefault();
-					$box.stop().animate({'left': left}, 50);
-					break;
-					case 40: //아래쪽
-					if (top+50 > h2top){
-						top = h2top;
-					} else {
-						top = top + 50;
-						//event.preventDefault();
-					};
-					$box.stop().animate({'top': top}, 50);
-					break;
-			};
-			//박스의 위치
-			//애니메이션이 시작하기 전에... 범위 계산이 끝나야 한다.
-			//박스의 위치가 윈도우보다 넘어가지 않게
-			//if ()
+					//박스의 위치
+					//애니메이션이 시작하기 전에... 범위 계산이 끝나야 한다.
+					//박스의 위치가 윈도우보다 넘어가지 않게
+					//if ()
 
-			$('.top').on('click', function(event){
-				event.preventDefault();
-				//$(window).scrollTop(0);
-				$('html,body').animate({'scrollTop': 0}, 500);
-			});
-	});	
+					$('.top').on('click', function(event){
+						event.preventDefault();
+						//$(window).scrollTop(0);
+						$('html,body').animate({'scrollTop': 0}, 500);
+					});
+			});	*/
 
 
 
@@ -135,7 +135,36 @@ jQuery(function($){
 	//$('a').append('<i class="fa fa-external-link"></i>');
 
 	//그러나 ... 외부 링크로 가는 것에만 추가되어야 한다...
-	$('a[href^="http:"]').attr('target','_blank').append('<i class="fa fa-external-link"></i>');
+	  //$('a[href^="http:"]').attr('target','_blank').append('<i class="fa fa-external-link"></i>');
+
+
+
+/*//탭 패널 제작
+	$('#tab .tabs').on('click', 'li a', function(event){
+		var $this = $(this);
+		$this.parent('li').siblings().removeClass('active').end().addClass('active');
+//탭 패널 선택 
+	//선택된 탭에 href값을 찾아라
+		var tabSelector = $this.attr('href');
+		$(tabSelector).siblings().removeClass('active').end().addClass('active');
+	});*/
+
+//탭 패널 제작
+	$('#tab .tabs').on('click', 'li a', function(event){
+		var $this = $(this);
+		$this.parent('li')
+			.siblings()
+			.removeClass('active')
+			.end()
+			.addClass('active');
+//탭 패널 선택 
+	//선택된 탭에 href값을 찾아라
+		var tabSelector = $this.attr('href');
+		$(tabSelector).siblings().removeClass('active').end().addClass('active');
+//탭 패널 선택 
+	});
+
+
 
 });
 //똑같은 코드는 가능하면 반복하지 않는 것이 좋다.
@@ -145,5 +174,4 @@ jQuery(function($){
 //$('ul:first').append('<li>새아이템</li>'); - 뒤에 추가됨
 //$('ul:first').prepend('<li>새아이템</li>'); - 앞에 추가됨
 //$('ul:first li:last-child').prependTo('ul:first'); - 있던걸 빼서 맨앞으로 옮긴다(넣는다).
-
 
